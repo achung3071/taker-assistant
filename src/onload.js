9b88678afb9eeda491cb8fb195ac499a9211ae93
Andrew Chung <andrewchung2022@u.northwestern.edu>
2019-08-28 17:50:21 +0900

    added file and slider loading

--- src/onload.js
+++ src/onload.js
@@ -3,6 +3,19 @@ window.addEventListener('load', () => {
   //UI Elements Setup
   resizeContent();
   window.addEventListener('resize', resizeContent);
+  //File and slider loading
+  document.querySelector('#file-load').addEventListener('change', fileLoader.bind(null, 'file'));
+  document.querySelector('#directory-load').addEventListener('change', fileLoader.bind(null, 'directory'));
+  document.addEventListener('keyup', e => {
+    let popupsInvisible = true;
+    document.querySelectorAll('.bg-modal').forEach(modal => {
+      popupsInvisible = (popupsInvisible && modal.style.display !== 'block');
+    });
+    if (popupsInvisible) { //if modals are not displayed
+      if (e.keyCode === 65) document.querySelector('#prev-image').click(); //a
+      else if (e.keyCode === 68) document.querySelector('#next-image').click(); //d
+    }
+  });
 });
 
 //Resize content div (under header)
